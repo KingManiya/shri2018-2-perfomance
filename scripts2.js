0a055b22577f97697a918974c1eab8d1bece4b72
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-20 04:16:32 +0300

    js optimization 1.7

--- scripts2.js
+++ scripts2.js
@@ -1,9 +1,9 @@
 const output = document.querySelector('.modal__value');
 const rangeSLider = document.querySelector('.adjust-bar.adjust-bar_theme_temp');
 
-rangeSLider.oninput = function() {
-    output.innerHTML = this.value > 0 ? '+' + this.value : this.value;
-}
+// rangeSLider.oninput = function() {
+//     output.innerHTML = this.value > 0 ? '+' + this.value : this.value;
+// }
 
 const arrowRightDevs = document.querySelector('.devices__paginator .paginator__arrow_right');
 
