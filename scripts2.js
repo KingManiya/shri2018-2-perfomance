7e5ca22042c516d4a901082ec4b71cea4b5a1cd8
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-20 04:51:03 +0300

    js optimization 1.13

--- scripts2.js
+++ scripts2.js
@@ -1,28 +1,9 @@
-const arrowRightDevs = document.querySelector('.devices__paginator .paginator__arrow_right');
-arrowRightDevs.addEventListener('click', function () {
-    const arrowLeftDevs = document.querySelector('.devices__paginator .paginator__arrow_left');
-    arrowLeftDevs.classList.toggle('paginator__arrow_disabled', false);
-    document.querySelector('.devices').scroll({
-        top: 0,
-        left: 1366,
-        behavior: 'smooth'
-    });
-});
-
-const arrowRightScens = document.querySelector('.scenarios__paginator .paginator__arrow_right');
-arrowRightScens.addEventListener('click', function () {
-    const arrowLeftScens = document.querySelector('.scenarios__paginator .paginator__arrow_left');
-    arrowRightScens.classList.add('paginator__arrow_disabled');
-    arrowLeftScens.classList.remove('paginator__arrow_disabled');
-    const scenarios = document.querySelector('.scenarios');
-    scenarios.scroll({
-        top: 0,
-        left: 645,
-        behavior: 'smooth'
-    });
-});
-
-const showModal = function(selector) {
-    document.querySelector(selector).classList.add('modal_open');
+function a(b) {
+    this.event.target.classList.add('paginator__arrow_disabled');
+    document.querySelector(b + '__paginator .paginator__arrow_left').classList.remove('paginator__arrow_disabled');
+    document.querySelector(b).scrollLeft = 1366;
+}
+function c(d) {
+    document.querySelector(d).classList.add('modal_open');
     document.querySelector('body').style.overflow = 'hidden';
 };
